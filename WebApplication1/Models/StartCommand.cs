74d4d57f31a23429413aff0c4d84f82ff3fdeadc
Ementree <47636110+Ementree@users.noreply.github.com>
2019-04-21 21:48:36 +0300

    newCommit

--- WebApplication1/Models/StartCommand.cs
+++ WebApplication1/Models/StartCommand.cs
@@ -8,6 +8,7 @@ namespace WebApplication1.Models.Commands
 {
     public class StartCommand : Command
     {
+        private List<string> various = new List<string>() { "привет!", "привет", "хей", "хей!", "hello", "hello!", @"/start" };
         public override string Name => @"/start";
 
         public override bool Contains(Message message)
@@ -15,19 +16,19 @@ namespace WebApplication1.Models.Commands
             if (message.Type != Telegram.Bot.Types.Enums.MessageType.Text)
                 return false;
 
-            return message.Text.Contains(this.Name);
+            return message.Text.Contains(various.Where(x => x == message.Text.ToLower()).First());
         }
 
         public override async Task Execute(Message message, TelegramBotClient botClient)
         {
             var chatId = message.Chat.Id;
-            await botClient.SendTextMessageAsync(chatId, "Hello", parseMode: Telegram.Bot.Types.Enums.ParseMode.Markdown);
+            await botClient.SendTextMessageAsync(chatId, "Привет!", parseMode: Telegram.Bot.Types.Enums.ParseMode.Markdown);
         }
     }
 
     public class DescriptionBotCommand : Command
     {
-        private List<string> various = new List<string>() {"Что ты умеешь?", "что умеешь?", @"/description", "расскажи о себе" };
+        private List<string> various = new List<string>() {"что ты умеешь?", "что умеешь?", @"/description", "расскажи о себе" };
         public override string Name => "Что ты умеешь?";
 
         public override bool Contains(Message message)
@@ -36,7 +37,7 @@ namespace WebApplication1.Models.Commands
                 return false;
 
             //return message.Text.Contains(this.Name);
-            return message.Text.Contains(various.Where(x => x == message.Text).First());
+            return message.Text.Contains(various.Where(x => x == message.Text.ToLower()).First());
         }
 
         public override async Task Execute(Message message, TelegramBotClient botClient)
