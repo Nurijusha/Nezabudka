78cb345228665e8db667874d6a95d2ecf5c9884e
Ementree <47636110+Ementree@users.noreply.github.com>
2019-04-21 14:09:57 +0300

    HelloCommand

--- WebApplication1/Models/StartCommand.cs
+++ WebApplication1/Models/StartCommand.cs
@@ -4,6 +4,25 @@ using Telegram.Bot.Types;
 
 namespace WebApplication1.Models.Commands
 {
+    public class HelloCommand : Command
+    {
+        public override string Name => @"Hello";
+
+        public override bool Contains(Message message)
+        {
+            if (message.Type != Telegram.Bot.Types.Enums.MessageType.Text)
+                return false;
+
+            return message.Text.Contains(this.Name);
+        }
+
+        public override async Task Execute(Message message, TelegramBotClient botClient)
+        {
+            var chatId = message.Chat.Id;
+            await botClient.SendTextMessageAsync(chatId, "Hello!", parseMode: Telegram.Bot.Types.Enums.ParseMode.Markdown);
+        }
+    }
+
     public class StartCommand : Command
     {
         public override string Name => @"/start";
