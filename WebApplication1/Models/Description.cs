9f50dc5c078f3ef1d597f1d7774e1aebfb358d48
Ementree <47636110+Ementree@users.noreply.github.com>
2019-04-22 16:35:07 +0300

    uh

--- WebApplication1/Models/Description.cs
+++ WebApplication1/Models/Description.cs
@@ -17,10 +17,7 @@ namespace NezabudkaHelperBot.Models.Commands
             if (message.Type != Telegram.Bot.Types.Enums.MessageType.Text)
                 return false;
 
-            return message.Text.Contains(various.Where(x => x == message.Text
-                .TrimEnd(new char[] { '!', ' ', '@', '~', '?' })
-                .ToLower())
-                .First());
+            return message.Text.Contains(various.Where(x => x == message.Text).First());
         }
 
         public override async Task Execute(Message message, TelegramBotClient botClient)
