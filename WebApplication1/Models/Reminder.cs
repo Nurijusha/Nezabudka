c74f20165e719198705b0f7cd7024d5d63cc45d1
Ementree <47636110+Ementree@users.noreply.github.com>
2019-04-27 20:56:29 +0300

    //?

--- WebApplication1/Models/Reminder.cs
+++ WebApplication1/Models/Reminder.cs
@@ -35,10 +35,10 @@ namespace NezabudkaHelperBot.Models.Commands
             var remind = new Remind(message);
             var tokenSource = new CancellationTokenSource();
             var token = tokenSource.Token;
-            Action<TelegramBotClient, Remind> Send = (client, r) =>//?
+            Action<TelegramBotClient, Remind> Send = (client, r) =>
                 {
                     var Id = r.Message.Chat.Id;
-                    client.SendTextMessageAsync(Id, r.Event).GetAwaiter().GetResult();
+                    client.SendTextMessageAsync(Id, r.Event).GetAwaiter().GetResult();//?
                 };
 
             if (remind.Date.CompareTo(DateTime.Now) < 0 || //?
